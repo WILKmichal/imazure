3af307113d4fb11297a5dc2c00530afd3004e84c
Ronan Royet <royet_r@etna-alternance.net>
2023-07-20 11:02:15 +0200

    Ajout du nombre d'images pour chaque tag

--- back/app/tags/routes.py
+++ back/app/tags/routes.py
@@ -1,11 +1,26 @@
 from app.tags import bp
+from app.extensions import db
 from app.models.tag import Tag
+from app.models.image import Image
 from flask import (jsonify, request,Blueprint)
 
 @bp.get('all')
 def tags():
-    tages =  [i.to_dict(rules = ('-images',)) for i in Tag.query.all()]
-    return jsonify(tages)
+    most_used = (
+    db.session.query(
+        Tag,
+        db.func.count(db.case(((Image.id.isnot(None), 1)), else_=0)).label('image_count')
+    )
+    .outerjoin(Tag.images)
+    .group_by(Tag.id)
+    .order_by(db.func.count(Image.id).desc())
+    .all()
+    )
+    for tag, image_count in most_used:
+        tag.image_count = image_count
+    
+    tags =  [tag.to_dict(rules = ('-images', 'image_count')) for tag, image_count in most_used]
+    return jsonify(tags)
 
 @bp.get('count/')
 def allImageCount():
