f5c4867d69167e15950a247ef8b1cec18af9afaf
Ronan Royet <royet_r@etna-alternance.net>
2023-09-02 16:47:35 +0200

    Add descrition column to DB and model

--- back/app/models/image.py
+++ back/app/models/image.py
@@ -2,11 +2,12 @@ from app.extensions import db
 from sqlalchemy_serializer import SerializerMixin
 
 class Image(db.Model, SerializerMixin):
-    serialize_only = ('id', 'title', 'url', 'name', 'tags')
+    serialize_only = ('id', 'title', 'url', 'name', 'tags', 'description')
     serialize_rules = ('-tags.images',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
+    description = db.Column(db.String(2048))
     url = db.Column(db.String(2048))
     name = db.Column(db.String(64))
     
